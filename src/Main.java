700b624fc4db8572f79d97ddac12741f19e6ee41
Rafael Lopes <rafaellopesdonascimento2607@gmail.com>
2023-07-02 20:17:51 -0300

    teste-dados-numericos

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java now");
+
+        byte variavelByte = -128;
+        System.out.println(variavelByte);
+        //bytes vão do -128 ate o 127
+        //short vão do -32768 ate 32767
+        // char do 0 ate o 65535, ele não aceita numeros negativos
+        //int guarda -2 bilhoes ate 2 bilhoes
+        //long -9tri ate 9 tri
+
+
+        byte variavelByte2 = 127;
+        System.out.println(variavelByte2);
+
+        int variavelInt = 2568497;
+        System.out.println(variavelInt);
+
+        short variavelShort = -32768 ;
+        System.out.println(variavelShort);
+
+        char variavelChar = 65535;
+        System.out.println(variavelChar);
+
+        long variavelLong = 100000;
+        System.out.println(variavelLong);
+    }
+}
